d4ca74d6eb31d445b245c4dd6712226b9bb1e484
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-10-23 14:23:29 +0700

    edit floodfill

--- graph/floodfill.py
+++ graph/floodfill.py
@@ -9,12 +9,12 @@ def floodfill(image, sr, sc, color):
     image[sr][sc] = color
     
     rows, cols = len(image), len(image[0])
-    visited = set()
+    # visited = set()
     directions = [[1, 0],[-1, 0],[0, 1],[0, -1]]
 
     def traverse(i, j):
         q = deque()
-        visited.add((i, j))
+        # visited.add((i, j))
         q.append((i, j))
         while q:
             cur_i, cur_j = q.popleft()
@@ -26,11 +26,12 @@ def floodfill(image, sr, sc, color):
                 # 3. i, j not in visited
                 if ((i>=0 and i<rows) and 
                     (j>=0 and j<cols) and 
-                    image[i][j] == start and 
-                    (i, j) not in visited):
+                    image[i][j] == start
+                    # (i, j) not in visited
+                    ):
                     image[i][j] = color
                     q.append((i, j))
-                    visited.add((i,j))
+                    # visited.add((i,j))
 
     traverse(sr, sc)
                 
