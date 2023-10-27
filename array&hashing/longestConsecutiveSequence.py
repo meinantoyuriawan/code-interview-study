d7416ae50b4db894fa8a612faf8bdb03989da73c
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-10-27 10:20:29 +0700

    longestconsecv2

--- array&hashing/longestConsecutiveSequence.py
+++ array&hashing/longestConsecutiveSequence.py
@@ -14,6 +14,22 @@ def longestConsecutive(nums):
         count = max(count, temp)
     return count
 
+# my answer, it is the same with a little bit adjustment in same element and traverse start stop point
+def longestConsec2(nums):
+    count = min(1, len(nums))
+    answer = 0
+    nums = sorted(nums)
+    for i in range(1, len(nums)):
+        if (nums[i] == nums[i-1] +1):
+            count +=1
+        elif (nums[i] == nums[i-1]):
+            continue
+        else:
+            answer = max(count, answer)
+            count = 1
+    
+    return max(answer, count)
+
 
 def run_testcase():
     input = [[100,4,200,1,3,2], [0,3,7,2,5,8,4,6,0,1]]
