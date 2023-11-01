ed49a3e4cf931699a2b38ce31050634ce558c8cb
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-01 19:08:10 +0700

    rotatedArr

---
+++ binary/findMinRotatedArr.py
@@ -0,0 +1,14 @@
+def findMin(nums):
+    # binary search
+    # 1. find the mid element
+    # 2. if mid > what to search, look in right half
+    # 3. if mid < what to search, look in left half
+        left = 0
+        right = len(nums)-1
+        while left<right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid +1
+            else :
+                right = mid
+        return(nums[right])
