5e32b6ee5a69e4a20a930ad5a6b8254a5d24c6d0
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-08 16:54:23 +0700

    testcase

--- slidingwindow/longestsubstring.py
+++ slidingwindow/longestsubstring.py
@@ -1,4 +1,7 @@
 def lengthofLongestSubstring(s):
+    # traverse through s
+    # if the current element is equal to the element in temp array, delete every element in leftest array, shift the left
+    # count the maximum between the current window and before the count
     temp = []
     l = 0
     res = 0
@@ -11,3 +14,19 @@ def lengthofLongestSubstring(s):
         res = max(res, r-l+1)
 
     return res
+
+
+def run_testcase():
+
+    s1 = ['abcabcbb', "bbbbb", "pwwkew"]
+    output = [3, 1, 3]
+    for i in range(len(s1)):
+        if (lengthofLongestSubstring(s1[i])) != output[i]:
+            print("Testcase %s failed, s1: %s, outcome: %s" % (i+1, 
+                s1[i], output[i]))
+            continue
+        print("Testcase %s success, s1: %s, outcome: %s" % (i+1, 
+                s1[i], output[i]))
+
+if __name__ == "__main__":
+    run_testcase()
