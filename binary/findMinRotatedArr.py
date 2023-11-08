41c013135ed068dcdadb9266f14624f07835fb9b
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-08 17:38:59 +0700

    testcase

--- binary/findMinRotatedArr.py
+++ binary/findMinRotatedArr.py
@@ -12,3 +12,19 @@ def findMin(nums):
             else :
                 right = mid
         return(nums[right])
+
+
+def run_testcase():
+
+    s1 = [[3,4,5,1,2], [4,5,6,7,0,1,2], [11,13,15,17]]
+    output = [1, 0, 11]
+    for i in range(len(s1)):
+        if (findMin(s1[i])) != output[i]:
+            print("Testcase %s failed, s1: %s, outcome: %s" % (i+1, 
+                s1[i], output[i]))
+            continue
+        print("Testcase %s success, s1: %s, outcome: %s" % (i+1, 
+                s1[i], output[i]))
+
+if __name__ == "__main__":
+    run_testcase()
--- binary/kokoEatBananas.py
+++ binary/kokoEatBananas.py
@@ -26,5 +26,24 @@ def minEatingSpeed(piles, h):
 
     return res
 
+# piles = [30,11,23,4,20], h = 5 k =30
+# piles = [30,11,23,4,20], h = 6 k =23
 
-print(minEatingSpeed([3,6,7,11], 8)) #k = 4
+# print(minEatingSpeed([3,6,7,11], 8)) #k = 4
+
+
+def run_testcase():
+
+    piles = [[3,6,7,11], [30,11,23,4,20], [30,11,23,4,20]]
+    h = [8, 5, 6]
+    output = [4, 30, 23]
+    for i in range(len(piles)):
+        if (minEatingSpeed(piles[i], h[i])) != output[i]:
+            print("Testcase %s failed, piles: %s, h: %s, outcome: %s" % (i+1, 
+                piles[i], h[i], output[i]))
+            continue
+        print("Testcase %s success, piles: %s, h: %s, outcome: %s" % (i+1, 
+                piles[i], h[i], output[i]))
+
+if __name__ == "__main__":
+    run_testcase()
