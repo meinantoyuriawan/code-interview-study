1829d09791218ef9f3fd0170a75a3776101582ef
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-13 15:36:34 +0700

    method

--- linkedlist/LL.py
+++ linkedlist/LL.py
@@ -33,7 +33,7 @@ class LL:
             new_node.next = self.head
             self.head = new_node
 
-    # kinda rigged but ok
+    # kinda broken but ok
     def insertMiddle(self, val, index):
         new_node = Node(val)
         current_node = self.head
@@ -51,3 +51,46 @@ class LL:
                 current_node.next = new_node
             else:
                 print("index not present")
+
+    
+    # remove
+    # remove first
+    def removeFirst(self):
+        if self.head == None:
+            return
+
+        self.head = self.head.next
+
+    # remove last
+    def removeLast(self):
+        if self.head == None:
+            return
+        
+        current_node = self.head
+        while(current_node.next):
+            current_node = current_node.next
+
+        current_node.next = None
+
+    # remove from index
+    def removeMiddle(self, index):
+        current_node = self.head
+        position = 0
+
+        if position == index:
+            self.removeFirst()
+        else:
+            while (current_node != None) and (position+1 != index):
+                position += 1
+                current_node = current_node.next
+
+            if current_node != None :
+                current_node.next = current_node.next.next
+            else:
+                print("index not present")
+
+    def printLL(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, " -> ")
+            current_node = current_node.next
