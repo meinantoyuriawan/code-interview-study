5f5284a532553923498c70541b86e61d70e73863
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-14 14:15:32 +0700

    detect cycle

---
+++ linkedlist/cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head):
+        s = set()
+        temp = head
+        while temp:
+            if temp in s:
+                return True
+
+            s.add(temp)
+            temp = temp.next
+
+        return False
