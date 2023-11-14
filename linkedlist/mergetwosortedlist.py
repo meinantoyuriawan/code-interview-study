dbf561cf4b217cbbd347a9d973568475fc7e3fdd
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-14 13:38:28 +0700

    2 sorted list

--- linkedlist/mergetwosortedlist.py
+++ linkedlist/mergetwosortedlist.py
@@ -7,18 +7,23 @@ class ListNode:
 # Iterative
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummy = node = ListNode()
+        dummy = ListNode()
+        tail = dummy
 
         while list1 and list2:
             if list1.val < list2.val:
-                node.next = list1
+                tail.next = list1
                 list1 = list1.next
             else:
-                node.next = list2
+                tail.next = list2
                 list2 = list2.next
-            node = node.next
+            tail = tail.next
 
-        node.next = list1 or list2
+        # node.next = list1 or list2
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
 
         return dummy.next
     
