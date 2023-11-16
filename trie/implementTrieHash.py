a098b6bd22de96e79c09b39a8f29a375e25406bb
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-16 15:22:45 +0700

    trie hash

---
+++ trie/implementTrieHash.py
@@ -0,0 +1,38 @@
+# implement Trie using hash instead of array of character. performs better in terms of time and space complexity
+# the main difference is in the tree that implemented using hash in every child node.
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False 
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
+        return curr.end
+    
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
+        return True
