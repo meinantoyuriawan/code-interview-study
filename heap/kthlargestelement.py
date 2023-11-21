84daea3c59770d38559e66739de5d70d154bf8d7
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-21 11:52:47 +0700

    heap

---
+++ heap/kthlargestelement.py
@@ -0,0 +1,15 @@
+import heapq
+class KthLargest:
+
+    def __init__(self, k, nums):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val):
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
+        return(self.minHeap[0])
