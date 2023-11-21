aaebffb335ae9c86d78952bf2374939cd22f11d0
meinantoyuriawan <meinantoyuriawan@gmail.com>
2023-11-21 14:16:05 +0700

    using maxheap

--- heap/lastStone.py
+++ heap/lastStone.py
@@ -15,6 +15,24 @@ def lastStone(stones):
         stones = stack
     return stones[0]
 
+def lastStonesMaxHeap(stones):
+    nStones = []
+    for i in stones:
+        nStones.append(i*-1)
+
+    heapq.heapify(nStones)
+
+    while len(nStones) > 1:
+        # compare two stones
+        first = heapq.heappop(nStones)
+        second = heapq.heappop(nStones)
+        if second>first:
+            heapq.heappush(nStones, first-second)
+    
+    nStones.append(0) # for the stones length <=2
+    return abs(nStones[0])
+
 num = [2,7,4,1,8,1]
 res = lastStone(num)
+res2 = lastStonesMaxHeap(num)
 print(res)
